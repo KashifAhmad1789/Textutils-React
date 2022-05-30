fd1bc850be87201c1b14653c7497d79cf5632a1f
Kashif Ahmad <89984617+KashifAhmad1789@users.noreply.github.com>
2022-05-30 21:16:17 +0530

    update

--- Text-Utils-React/src/App.js
+++ Text-Utils-React/src/App.js
@@ -39,6 +39,14 @@ function App() {
       // setInterval(() => {
       //   document.title = 'Install TextUtils Now';
       // }, 1500);
+
+      
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
       
     }
     else{
